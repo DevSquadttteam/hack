c4a2a6b9102f4454e256a6ed59d4ad6f69e95ba7
ogerz3 <polatbekismoilov17@gmail.com>
2025-09-13 14:53:09 +0500

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,15 @@ import App from './App.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import Landing from './pages/Landing.jsx';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/', // Теперь главная — Landing
+    element: <Landing />,
+  },
+  {
+    path: '/app', // защищённая часть
     element: (
       <PrivateRoute>
         <App />
